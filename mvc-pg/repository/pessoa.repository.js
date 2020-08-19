137b7896eb7824230b63443f889b73b4d68cedd3
Prof. Libório Júnior <prof@liboriojunior.eti.br>
2020-08-18 21:51:02 -0300

    Aula 18/08/2020

--- mvc-pg/repository/pessoa.repository.js
+++ mvc-pg/repository/pessoa.repository.js
@@ -1,8 +1,9 @@
 const conn = require('../pg-connection')
 
-const pessoaQuery = 'select pessoa.*, cidade.nome as cidade_nome, cidade.uf as cidade_uf '+
+const pessoaQuery = 'select pessoa.*, cidade.nome as cidade_nome, cidade.uf as cidade_uf, usuario.username '+
                     'from pessoa '+
-                    'left join cidade on cidade.id = pessoa.cidade_id';
+                    'left join cidade on cidade.id = pessoa.cidade_id '+
+                    'inner join usuario on usuario.id = pessoa.usuario_id';
 
 //Camada de Modelo
 module.exports = {
@@ -16,13 +17,13 @@ module.exports = {
     },
 
     create: ( pessoa ) => {
-        return conn.query('insert into pessoa(nome, email, fone, endereco, cidade_id) values ($1,$2,$3,$4,$5) returning *', 
-                          [pessoa.nome, pessoa.email, pessoa.fone, pessoa.endereco, pessoa.cidade.id]);
+        return conn.query('insert into pessoa(nome, email, fone, endereco, cidade_id, usuario_id) values ($1,$2,$3,$4,$5,$6) returning *', 
+                          [pessoa.nome, pessoa.email, pessoa.fone, pessoa.endereco, pessoa.cidade.id, pessoa.usuario.id]);
     },
 
     update: ( pessoa ) => {
-        return conn.query('update pessoa set nome = $1, email = $2, fone = $3, endereco = $4, status = $5, cidade_id = $6 where id = $7 returning *', 
-                          [pessoa.nome, pessoa.email, pessoa.fone, pessoa.endereco, pessoa.status, pessoa.cidade.id, pessoa.id]);
+        return conn.query('update pessoa set nome = $1, email = $2, fone = $3, endereco = $4, status = $5, cidade_id = $6, usuario_id = $7 where id = $8 returning *', 
+                          [pessoa.nome, pessoa.email, pessoa.fone, pessoa.endereco, pessoa.status, pessoa.cidade.id, pessoa.usuario.id, pessoa.id]);
     },
 
     delete: ( id ) => {
--- mvc-pg/routes/index.js
+++ mvc-pg/routes/index.js
@@ -4,8 +4,10 @@ const routes = new express.Router();
 //Importa as rotas da aplicação
 const cidadeRoute = require('../routes/cidade.route');
 const pessoaRoute = require('../routes/pessoa.route');
+const usuarioRoute = require('../routes/usuario.route');
 
 routes.use('/cidades', cidadeRoute);
 routes.use('/pessoas', pessoaRoute);
+routes.use('/account', usuarioRoute);
 
 module.exports = routes;
